741bb048c30cd58c74362c739e9578c028f89396
M.Haseeb <haseebashfaq700@gmail.com>
2026-07-02 15:58:37 +0500

    Add files via upload

---
+++ PDF-Merger .py
@@ -0,0 +1,13 @@
+from pypdf import PdfWriter
+
+merger = PdfWriter()
+files = [
+    "Enter your PDF's here you want to merge"
+]
+for file in files:
+    merger.append(file)
+
+name  = input("Enter the name you want to give to new PDF :")
+
+merger.write(name)
+merger.close()
